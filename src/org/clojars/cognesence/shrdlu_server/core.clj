432ef25f7294215c4e9403e4f4f8038da81be53d
Saul Johnson <saul.a.johnson@gmail.com>
2017-06-30 21:29:32 +0100

    Begin code tidy

--- src/org/clojars/cognesence/shrdlu_server/core.clj
+++ src/org/clojars/cognesence/shrdlu_server/core.clj
@@ -8,163 +8,107 @@
             [org.clojars.cognesence.ops-search.core :refer :all])
   (:gen-class))
   
-;this file contains high level glue & wrappers and their
-;associalted functions...
-;
-;    uiwin -- the native Java window controls
-;    comms -- the socket link to netlogo
-;
-;===========================================================
-;
-; load order...
-;    matcher
-;    ops-search
-;    this file
-;    strips-search
-;    block-fns
-;    nlp
-;
-;==========================================================
+;====================================
+; core
+;====================================
 
-
-
-
+(defn set-atom! [atm x]
+  (swap! atm (fn [_] x)))
+  
 ;====================================
 ; window manufacture & control
 ;====================================
-
-(import '(java.awt Font))
-
-(declare
-  set-atom!
-  add-window
-  )
-
-;____ builders ______________________
+  
+(import '(java.awt Font TextArea)
+        '(javax.swing Box BoxLayout JFrame JLabel))
 
 (def uiwin (atom {}))
 
-(defn setup-ui-windows []
+(defn add-window
+  ""
+  ([box name label] (add-window box name label 7 50))
+  ([box name label row col]
+    (let [ta (new TextArea row col)]
+      (.setFont ta (new Font Font/MONOSPACED Font/PLAIN 12))
+      (.add box (new JLabel label))
+      (.add box ta)
+      (swap! uiwin assoc name ta))))
+
+(defn setup-ui-windows 
+  ""
+  []
   (set-atom! uiwin {})
-  (let [f   (new javax.swing.JFrame)
-        box (new javax.swing.Box javax.swing.BoxLayout/Y_AXIS)
-        ]
+  (let [f (new JFrame)
+        box (new Box BoxLayout/Y_AXIS)]
     (.setSize f 500 500)
     (.setVisible f true)
     (.add f box)
-
     (add-window box :dbg  "reasoning details")
     (add-window box :comm "communication")
-    (add-window box :bdat "belief system of planner")
-    ))
-
-
-(defn add-window
-  ([box name label] (add-window box name label 7 50))
-  ([box name label row col]
-    (let [ta (new java.awt.TextArea row col)]
-      (.setFont ta (new Font Font/MONOSPACED Font/PLAIN 12))
-      (.add box (new javax.swing.JLabel label))
-      (.add box ta)
-      ;; if you want cognesence colors
-;      (.setBackground ta (new java.awt.Color 13 58 79))
-;      (.setForeground ta java.awt.Color/WHITE)
-      ;; finally
-      (swap! uiwin assoc name ta)
-      )))
-
-
-;____ window dispatch ___________________
+    (add-window box :bdat "belief system of planner")))
 
 (def win-mode true)
 
-;(defn ui-out
-;  [& r]
-;  (if-not win-mode (apply println r)
-;    (let [[win & r] r]
-;      (.append (win @uiwin) (write r :stream nil))
-;      (.append (win @uiwin) "\n")
-;      )))
-
 (defn ui-out
+  ""
   [win & r]
   (if-not win-mode (apply println r)
-   ;; (let [[win & r] r]
     (do
       (doseq [x r]
         (cond
           (nil? x)
           (.append (win @uiwin) "nil")
-
           (coll? x)
           (.append (win @uiwin) (write x :stream nil))
-
           (symbol? x)
           (.append (win @uiwin) (str x))
-
           :else
-          (.append (win @uiwin) x)
-          )
+          (.append (win @uiwin) x))
         (.append (win @uiwin) " "))
-      (.append (win @uiwin) "\n")
-      )))
-
+      (.append (win @uiwin) "\n"))))
 
 (defn ui-broadcast [stuff]
+  ""
   (if-not win-mode (apply println stuff)
     (doseq [io (keys @uiwin)]
-      (ui-out io stuff))
-    ))
-
-
+      (ui-out io stuff))))
 
 (defn ui-set
+  ""
   [& r]
   (if-not win-mode (apply println r)
     (let [[win & r] r]
-      (.setText (win @uiwin) (write r :stream nil))
-      )))
+      (.setText (win @uiwin) (write r :stream nil)))))
 
-
-(defn clear-ui []
+(defn clear-ui 
+  ""
+  []
   (if win-mode
     (doseq [io (keys @uiwin)]
-      (.setText (io @uiwin) ""))
-    ))
-
-
-
+      (.setText (io @uiwin) ""))))
 
 ;====================================
 ; socket manufacture & control
 ;====================================
 
-
 (import '(java.net ServerSocket Socket SocketException)
-  '(java.io InputStreamReader OutputStreamWriter)
-  '(clojure.lang LineNumberingPushbackReader))
-
-
-;___ active socket is used as a global _____________
-
+        '(java.io InputStreamReader OutputStreamWriter)
+        '(clojure.lang LineNumberingPushbackReader))
+ 
 (def shrdlu-comms false)
 
 (defn startup-server
+  ""
   [port]
   (let [ss (new ServerSocket port)]
     (println "advertising " ss)
     (try (let [s (.accept ss)]
            (println "socket accepted " s)
-
-           { :sock s
-             :inp  (new LineNumberingPushbackReader
-                     (new InputStreamReader (.getInputStream s)))
-             :outp (new OutputStreamWriter (.getOutputStream s))
-             }
-           )
-      (catch SocketException e))
-    ))
-
+           {:sock s
+            :inp (new LineNumberingPushbackReader
+                  (new InputStreamReader (.getInputStream s)))
+            :outp (new OutputStreamWriter (.getOutputStream s))})
+      (catch SocketException e))))
 
 (defn socket-write
   "low-level socket writer"
@@ -1087,10 +1031,6 @@
           )))
     ))
 
-
-
-(defn set-atom! [atm x]
-  (swap! atm (fn [_] x)))
 
 
 (let [n (atom 0)]
