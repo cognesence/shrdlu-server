c1f903fbb4d06d51cce7ad4848ade256ffae0ce2
Saul Johnson <saul.a.johnson@gmail.com>
2017-06-30 21:07:03 +0100

    Fix dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojars.cognesence/matcher "1.0.1"]
-                 [org.clojars.cognesence/opsearch "1.0.0"]]
+                 [org.clojars.cognesence/ops-search "1.0.1"]]
   :main ^:skip-aot org.clojars.cognesence.shrdlu-server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
