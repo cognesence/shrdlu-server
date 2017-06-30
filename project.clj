ea041fee7acc202db9d5ad80721ad662d6641bd9
Saul Johnson <saul.a.johnson@gmail.com>
2017-06-30 16:17:27 +0100

    Update project file

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject shrdlu-server "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "The server side of the SHRDLU example used at ELS 2015 and Game On."
+  :url "https://github.com/cognesence/shrdlu-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot shrdlu-server.core
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojars.cognesence/matcher "1.0.1"]
+                 [org.clojars.cognesence/opsearch "1.0.0"]]
+  :main ^:skip-aot org.clojars.cognesence.shrdlu-server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
